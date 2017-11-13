545f94b3345e4ddad2fcd773447a05da92ee502f
Scott Krelo Sr <appengine@americaforanimals.org>
2017-11-12 18:47:12 -0700

    Full Commit w/ node modules

--- config/cache.php
+++ config/cache.php
@@ -51,11 +51,11 @@ return [
 
         'memcached' => [
             'driver' => 'memcached',
-            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-            'sasl' => [
-                env('MEMCACHED_USERNAME'),
-                env('MEMCACHED_PASSWORD'),
-            ],
+            //'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
+           // 'sasl' => [
+           //     env('MEMCACHED_USERNAME'),
+           //     env('MEMCACHED_PASSWORD'),
+           // ],
             'options' => [
                 // Memcached::OPT_CONNECT_TIMEOUT  => 2000,
             ],
